a46fd8321a064279bfee56d385d6833ad91f8ca1
Pritesh Shinde <priteshs@prdxn.com>
2020-03-26 00:18:19 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import openSocket from 'socket.io-client';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const socket = openSocket('http://localhost:8080');
+socket.on('mailSend', data => console.log(data));
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
